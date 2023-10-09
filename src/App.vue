d45f69ad97bdecf88a929f9bf6a7f468f7ccbdb1
IharDorinel <ihardaronin@gmail.com>
2023-10-09 16:40:09 +0300

    create project

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <the-header title='RememberMe'></the-header>
+  <the-resources></the-resources>
+</template>
+
+<script>
+
+import TheHeader from './components/layouts/TheHeader';
+import TheResources from './components/learning-resources/TheResources';
+export default {
+  components: {
+    TheResources,
+    TheHeader,
+
+  },
+
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
